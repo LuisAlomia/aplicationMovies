de8cb48405d3c9224e1439d9353651a1adc9632f
Luis Alomia <luisfidel44@gmail.com>
2023-03-25 17:20:37 -0500

    Initial configuration

---
+++ appReact/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen">
+      <h1 className="text-fuchsia-400 text-4xl text-center">Hello Mundo</h1>
+    </div>
+  );
+}
+
+export default App;
